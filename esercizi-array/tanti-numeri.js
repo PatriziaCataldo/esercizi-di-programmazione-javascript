1bd37121580728415e5a9dbf3cc50c7cd85fecb3
PatriziaCataldo <93885156+PatriziaCataldo@users.noreply.github.com>
2022-02-21 16:41:07 +0100

    Update tanti-numeri.js

--- esercizi-array/tanti-numeri.js
+++ esercizi-array/tanti-numeri.js
@@ -12,3 +12,31 @@
 
   http://www.imparareaprogrammare.it
 */
+let quanti=prompt("quanti sono i numeri di cui vuoi calcolare la media?")
+
+let vettore= new Array()
+vettore.length=quanti
+for ( let i=0;i<vettore.length;i++){
+vettore[i]=Math.floor(Math.random()*50+1)
+
+}console.log(vettore)
+let media=0
+for (let i=0;i<vettore.length;i++){
+media+=Number(vettore[i])/vettore.length
+}
+
+console.log("il valore medio dei numeri è :" +media)
+
+let vet=Array()
+let x=0
+let y=0
+while (x<vettore.length){
+if (vettore[x]<media){
+vet[y]=vettore[x]
+y++
+}x++
+}console.log( "I numeri inferiori al valore medio sono:    " +vet)
+
+
+
+
