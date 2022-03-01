d19a7be319e326fb61899a94c9d0a33e995927d6
PatriziaCataldo <93885156+PatriziaCataldo@users.noreply.github.com>
2022-03-01 16:53:01 +0100

    Update rombo-che-stampa.js

--- esercizi-cicli/rombo-che-stampa.js
+++ esercizi-cicli/rombo-che-stampa.js
@@ -16,3 +16,49 @@
   
   http://www.imparareaprogrammare.it
 */
+let rombo=""
+let input1=prompt("inserisci un numero dispari")
+console.log(input1)
+let input2=input1
+console.log(input2)
+let ast=1
+
+
+while (input1>0){
+ let righe1=Math.floor(input1/2)
+for ( let i=0;i<righe1;i++){
+rombo+=" "
+}
+let j=ast
+while(j>0){
+rombo+="*"
+j--
+}
+rombo+="\n"
+ast+=2
+input1-=2
+}
+
+
+input1=input2
+let ast2=input1-2
+while (input1>0){
+let tab=1
+for (let i=0;i<tab;i++){
+rombo+=" "
+ }
+ tab++
+
+let j=ast2
+while(j>0){
+rombo+="*"
+j--
+}
+rombo+="\n"
+input1-=2
+ast2-=2
+}
+
+
+
+alert(rombo)
