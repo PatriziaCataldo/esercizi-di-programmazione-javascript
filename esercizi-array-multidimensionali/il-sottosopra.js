698740fe88a1b398880002a5db8d951a3f574ea8
PatriziaCataldo <93885156+PatriziaCataldo@users.noreply.github.com>
2022-05-31 20:22:01 +0200

    Update il-sottosopra.js

--- esercizi-array-multidimensionali/il-sottosopra.js
+++ esercizi-array-multidimensionali/il-sottosopra.js
@@ -16,13 +16,35 @@
       ]
 
   http://www.imparareaprogrammare.it
-*/
-let input=[[1,2],[3,4],[5,6]]
-
-let output= []
-for (let x=0;x<input.length-1;x++){
-
-  for ( let i=0;i<input.length;i++){
-    output[i]=input[i][x]
-  }console.log(output)
+*/let righe=prompt("Quante righe di numeri consecutivi vuoi inserire?")
+let colonne=prompt("Quante colonne per ogni riga?")
+let vettore=[]
+for (let x=0;x<righe;x++){
+vettore[x]=[]
 }
+
+let k=0
+for(let i=0;i<righe;i++){
+for(let j=0;j<colonne;j++){
+k++
+vettore[i][j]=k
+}console.log(vettore[i])
+
+}
+
+
+let tran=[]
+for (x=0;x<colonne;x++){
+
+tran[x]=[]
+}
+
+
+for (let y=0;y<colonne;y++){
+for ( let x=0;x<righe;x++){
+
+tran[y][x]=vettore[x][y]
+}
+}console.log(tran)
+
+
