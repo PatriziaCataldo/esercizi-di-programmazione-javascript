2b7fc286d1abc9d14b615cd68b1f91cc7e8cb589
PatriziaCataldo <93885156+PatriziaCataldo@users.noreply.github.com>
2022-06-09 16:07:36 +0200

    Update vinci-tu.js

--- esercizi-cicli/vinci-tu.js
+++ esercizi-cicli/vinci-tu.js
@@ -14,3 +14,26 @@
 
   http://www.imparareaprogrammare.it
 */
+let gioc1=0
+let gioc2=0
+let tiro1=0
+for (let x=0;x<=4;x++){
+tiro1=Math.round(Math.random()*5+1)
+gioc1+=tiro1
+console.log("il giocatore 1 estrae il numero:"+tiro1)
+tiro2=Math.round(Math.random()*5+1)
+gioc2+=tiro2
+console.log("Il giocatore 2 estrae il numero:"+tiro2)
+
+
+}console.log("Il giocatore 1 totalizza punti:"+gioc1)
+console.log(" il giocatore 2 totalizza punti:"+gioc2)
+let van=0
+if(gioc1>gioc2){
+van=gioc1-gioc2
+console.log("Il giocatore 1 vince con: "+"  " +van+ "   punti di vantaggio")
+}else if(gioc1<gioc2){
+van=gioc2-gioc1
+console.log("Il giocatore 2 vince con :"+"  "+ van+"   punti di vantaggio")
+}else
+console.log("i giocatori pareggiano")
